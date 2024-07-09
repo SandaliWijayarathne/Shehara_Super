2b0bdf6cdd2576eca75705fa7c2fd965431454b1
Sans <157110796+SandaliWijayarathne@users.noreply.github.com>
2024-07-09 09:05:55 +0530

    navbar

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -5,8 +5,7 @@ const ProductSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     category: { type: String, required: true },
-    new_price: { type: Number, required: true },
-    old_price: { type: Number, required: true },
+    price: { type: Number, required: true },
     date: { type: Date, default: Date.now },
     available: { type: Boolean, default: true },
 });
