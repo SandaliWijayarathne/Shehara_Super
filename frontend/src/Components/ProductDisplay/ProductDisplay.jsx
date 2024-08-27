770a1ab47f12642dd3f1df2605209fab2ad5fef0
ThisarangiThathsara <thisarangi03@gmail.com>
2024-08-27 20:02:59 +0530

    product display update

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,26 @@
 import React, { useContext } from 'react';
 import './ProductDisplay.css';
 import { ShopContext } from '../../Context/ShopContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductDisplay = (props) => {
     const { product } = props;
     const { addToCart } = useContext(ShopContext);
+
+    const handleAddToCart = (productId) => {
+        addToCart(productId);
+        toast.success(`${product.name} has been added to your cart!`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            className: 'toastify-custom', // Add custom class here
+        });
+    }
 
     return (
         <div className='productdisplay'>
@@ -23,11 +39,12 @@ const ProductDisplay = (props) => {
                         <option value="2kg">2 kg</option>
                     </select>
                 </div>
-                <button onClick={() => { addToCart(product.id) }}>Add to Cart</button>
+                <button onClick={() => handleAddToCart(product.id)}>Add to Cart</button>
                 <div className="productdisplay-right-description">
                     {product.description}
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }
