ae2e0db3ec26006eba6be03c0e6c923492d2b24d
VisaraDeAlwis <157110584+VisaraDeAlwis@users.noreply.github.com>
2024-08-27 19:20:10 +0530

    banner updated

---
+++ frontend/src/Components/Assets/banner_images.js
@@ -0,0 +1,9 @@
+
+import image1 from './Banner/D1.png'
+import image2 from './Banner/D2.png';
+import image3 from './Banner/D3.png';
+import image4 from './Banner/D4.png';
+
+const bannerImages = [image1, image2, image3, image4];
+
+export default bannerImages;
--- frontend/src/Components/Collections/Collections.jsx
+++ frontend/src/Components/Collections/Collections.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Collections.css';
 import collections from '../Assets/collections';
 import Item from '../Item/Item';
-import bannerImage from '../Assets/Desktop - 1.png';
+import bannerImages from '../Assets/banner_images'
 
 const NewCollections = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % bannerImages.length);
+    }, 4000); 
+
+    return () => clearInterval(intervalId); // Cleanup on component unmount
+  }, []);
+
   return (
     <div className="new-collections">
-      <img src={bannerImage} alt="Banner" className="banner-image" />
+      <div className="banner-container">
+        <div className="banner-slider" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+          {bannerImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Banner ${index}`}
+              className="banner-image"
+            />
+          ))}
+        </div>
+      </div>
       <div className="collections-container">
         <div className="collections">
           {collections.map((item, i) => (
