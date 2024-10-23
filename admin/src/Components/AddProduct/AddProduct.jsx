52561ba49880cfbd83b7297e3ec769ed14be968b
Sans <157110796+SandaliWijayarathne@users.noreply.github.com>
2024-10-23 15:45:22 +0530

    Last

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -3,8 +3,8 @@ import './AddProduct.css';
 import upload_area from '../../assets/upload_area.svg';
 import { message } from 'antd'; // Import message from antd
 
-const S_URL = "localhost";
 
+URL = "13.51.121.50"
 const AddProduct = () => {
   const [image, setImage] = useState(null);
   const [productDetails, setProductDetails] = useState({
