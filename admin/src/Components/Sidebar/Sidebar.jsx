a6f5499fe84b57612565ff7901a3269c7645615a
Sans <157110796+SandaliWijayarathne@users.noreply.github.com>
2024-10-23 14:48:32 +0530

    updated

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -4,17 +4,11 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <Link to={'/dashboard'} className='sidebar-link'>
-        <div className="sidebar-item">Dashboard</div>
-      </Link>
       <Link to={'/addproduct'} className='sidebar-link'>
         <div className="sidebar-item">Add Product</div>
       </Link>
       <Link to={'/listproduct'} className='sidebar-link'>
         <div className="sidebar-item">Product List</div>
-      </Link>
-      <Link to={'/orders'} className='sidebar-link'>
-        <div className="sidebar-item">Orders List</div>
       </Link>
       <Link to={'/addbanner'} className='sidebar-link'>
         <div className="sidebar-item">Banners</div>
